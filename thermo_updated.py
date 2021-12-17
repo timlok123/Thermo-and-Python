b054c4e84b94c5b215adf0d2fe4f9fcdd057c9f7
Justin Chau <u3577808@connect.hku.hk>
2021-12-17 18:46:58 +0800

    init

--- thermo_updated.py
+++ thermo_updated.py
@@ -24,7 +24,7 @@ class sim():
     X = 2
     Y = 2
 
-    def __init__(self, dt = 1E-2, Num = 10):
+    def __init__(self, dt = 1E-5, Num = 10):
         self.dt = dt
         self.Num = Num
         ##
@@ -32,34 +32,51 @@ class sim():
         ##
     
     def collision_dection(self):
-        print("Hello World")
+        
+        ##detect if the particles collide with the wall 
+        for particle in self.particles:
+            x,y = particle.position
+            if(self.X/2< x+particle.radius) or (-self.X/2> x-particle.radius):
+                particle.velocity[0] *=-1
+            if(self.Y/2< y+particle.radius) or (-self.Y/2> y-particle.radius):
+                particle.velocity[1] *=-1
+
+            id1 = particle.id
+
+        ## Check if the particles collide with other particle 
+            for particle2 in self.particles:
+                if(id1 == particle2.id):
+                    continue
+
+                else:
+                    
+
+                
+
 
     def increment(self):
         self.collision_dection()
         for i in self.particles:
-            i += i.position + i.velocity*self.dt
+            i.position += i.velocity*self.dt
 
     def particle_position(self):
-        return [ i.position for i in self.particles]
+        return [i.position for i in self.particles]
 
     def particle_colour(self):
-        return [ i.colour for i in self.particles]
+        return [i.colour for i in self.particles]
 
 
 sim = sim()
 
 decided_v = 1000
-
 ## Set the position and velocity of the particles 
-for particle in sim.particle:
+for particle in sim.particles:
     particle.position = np.random.uniform([-sim.X/2,-sim.Y/2], [sim.X/2,sim.Y/2], size =2) 
-    particle.velocity = np.random.uniform([-decided_v,-decided_v], [decided_v,decided_v], size =2)
+    particle.velocity = np.random.uniform([-decided_v,-decided_v], [decided_v,decided_v],size2) 
 ##
 
-    
 
 ## plot code 
-
 fig, ax = plt.subplots()
 
 scatter = ax.scatter([],[])
@@ -71,11 +88,11 @@ def init():
 
 def update(frame):
     sim.increment()
-    scatter.set_offsets(np.array(sim.particle_positions()))
+    scatter.set_offsets(np.array(sim.particle_position()))
     return scatter, 
 
-ani = FuncAnimation(fig, update, frame=range(1200), init_func = init, blit = True, interval = 1/30, repeat = True)
-
+ani = FuncAnimation(fig, update, frames=range(1200), init_func = init, blit = True, interval = 1/30, repeat = False)
+ 
 plt.show()
 
 
